4d04f95d860910b4f48f64ba67dc322f7b4d26f4
tkhe <violin1229@163.com>
2020-06-19 01:26:04 +0800

    use read_image to read semantic segmentation

--- tkdet/data/dataset_mapper.py
+++ tkdet/data/dataset_mapper.py
@@ -3,8 +3,6 @@ import logging
 
 import numpy as np
 import torch
-from PIL import Image
-from fvcore.common.file_io import PathManager
 
 from . import detection_utils as utils
 from . import transforms as T
@@ -107,9 +105,7 @@ class DatasetMapper(object):
             dataset_dict["instances"] = utils.filter_empty_instances(instances)
 
         if "sem_seg_file_name" in dataset_dict:
-            with PathManager.open(dataset_dict.pop("sem_seg_file_name"), "rb") as f:
-                sem_seg_gt = Image.open(f)
-                sem_seg_gt = np.asarray(sem_seg_gt, dtype="uint8")
+            sem_seg_gt = utils.read_image(dataset_dict.pop("sem_seg_file_name"), "L").squeeze(2)
             sem_seg_gt = transforms.apply_segmentation(sem_seg_gt)
             sem_seg_gt = torch.as_tensor(sem_seg_gt.astype("long"))
             dataset_dict["sem_seg"] = sem_seg_gt
